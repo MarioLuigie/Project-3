d5235ce319c14050e4435984c5e409ced51dcb35
Mario Luigie <mk.lotocki@gmail.com>
2022-11-20 18:56:14 +0100

    add function changeBackground in main.js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 window.onload = () => {
-  let number = 5;
-  let string = "Mike";
-  let result = number + string;
-  
-  alert(result); //plus concat number and string
 
+    let box = document.querySelector(".container");
+    let title = document.querySelector("h1");
+    let logical = true;
+    
+    function changeBackground() {
+        logical = !logical;
+        box.style.backgroundColor = logical ? "black" : "greenyellow";
+        title.style.color = logical ? "white" : "black";
+    };
+
+    document.addEventListener("click", changeBackground);
 };
